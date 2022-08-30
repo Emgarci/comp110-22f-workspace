7ee227e7b7e00ddb1114dcb9fa699942262194bd
Emily Garcia <emilyma@ad.unc.edu>
2022-08-29 23:36:28 -0400

    “Progress on Exercise 1”

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,13 @@
+"""EX01 - Chardle -A cute step toward wordle"""
+__author__ ="730350205"
+the_word: str = input("Enter a 5-character word: ")
+the_character: str = input("Enter a single character: ")
+print ("Searching for " + the_character + " in " + str(the_word))
+print (the_character + " found at index 1")
+
+
+
+
+
+
+
